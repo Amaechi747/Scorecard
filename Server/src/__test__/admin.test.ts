ce75983f848bc0134f669f288050130425979cf4
Gavin <longgaving@gmail.com>
2022-08-10 03:15:34 +0100

    viewAll admin test case

--- Server/src/__test__/admin.test.ts
+++ Server/src/__test__/admin.test.ts
@@ -1,8 +1,9 @@
 import { exec } from 'child_process';
 import mongoose,{Error} from 'mongoose';
 // import { describe, it } from 'node:test';
+import application from "../app";
 import { beforeAll, afterEach, afterAll, describe, it, expect } from '@jest/globals'
-import supertest from 'supertest';
+import request from 'supertest';
 import {fakeAdmin, dbConnect, dbDisconnect, dropCollections} from '../database/fakeDB/admin';
 import Admin from '../models/adminSchema';
 
@@ -11,7 +12,9 @@ import Admin from '../models/adminSchema';
 // Database Connections
 beforeAll(async () => await dbConnect());
 afterAll(async () => await dbDisconnect());
-afterEach( async () => await dropCollections)
+afterEach(async () => await dropCollections)
+
+
 
 describe('Create Admin Models', () => {
     it('Should create an new Admin', async () => {
@@ -26,10 +29,32 @@ describe('Create Admin Models', () => {
             firstName: "Moses",
             password: 1234,
         }
-        const newAdmin = new Admin({...fake});
-        expect(await newAdmin.save()).toThrow(Error.ValidationError);
+        const newAdmin = new Admin({ ...fake });
+        try {
+            await newAdmin.save()
+        } catch (error) {
+            expect(error).toBeDefined()
+        }
+        // expect().toThrowError(Error.ValidationError);
     })
 })
 
+describe('viewAll Admin Models', () => {
+    it('it should display all Admins when returned true', async () => {
+         const res = await (await request(application).get('/admin'))
+            expect(res.status).toBe(200);
 
+    })
+    
+
+})
+
+
+
+
+
+
+function app(app: any) {
+    throw new Error('Function not implemented.');
+}
 
