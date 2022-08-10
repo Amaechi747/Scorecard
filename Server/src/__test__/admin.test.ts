08babce0cececf58bc6a83da7d0dcdbbbc51382a
Ugwuanyi Love Ekene <49119675+Hubnotch@users.noreply.github.com>
2022-08-10 08:50:43 +0100

    Updated test case for stack creation

--- Server/src/__test__/admin.test.ts
+++ Server/src/__test__/admin.test.ts
@@ -183,7 +183,7 @@ describe('viewAll Admin Models', () => {
     
 
 })
-
+//test for stack creation by  super admin
 describe('create stack',()=>{
     it('it should return true when stack is created',async ()=>{
         const res = await(await request(app).get('/admin/create_stack'))
